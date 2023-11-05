3ce0004ddea25cf76bd94a17fd9c69706a7e6ca6
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-05 18:34:22 +0600

    added everything to change this project completely and also updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Live Link
-Live Link : https://64f2a0503dd92a3508fcf9f7--admirable-faun-a457d7.netlify.app/
+Live Link : https://kmfoysal06.github.io/kmdb/
--- Script/index.js
+++ Script/index.js
@@ -1,19 +1,54 @@
 let categories = [];
 
 // Tab section
-
+let page = 1;
+let pageNoP = document.querySelector(".page-no");
+let nextPageBtn = document.querySelector(".next-page-btn");
+let prevPageBtn = document.querySelector(".prev-page-btn");
+nextPageBtn.addEventListener("click",(e)=>{
+  if(page === 1){
+    prevPageBtn.disabled = true;
+  }else{
+    prevPageBtn.disabled = false;
+  }
+  page ++;
+  handleLoadPost(e.target.id,page);
+})
+prevPageBtn.addEventListener("click",(e)=>{
+  if(page === 1){
+    prevPageBtn.disabled = true;
+  }else{
+    prevPageBtn.disabled = false;
+  }
+  page --;
+  handleLoadPost(e.target.id,page);
+})
 const handleCategory = async () => {
 
-  const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
-  const data = await response.json();
-  categories = data.data;
+  categories = [{
+    category:"Action",
+    id:28
+  },
+  {
+    category:"Comedy",
+    id:35
+  },
+   {
+    category:"Horror",
+    id:27
+  },
+   {
+    category:"Adventure",
+    id:12
+  },
+  ];
 
   const categoryContainer = document.getElementById("category-container");
 
   categories.slice(0, 4).forEach((category) => {
     const div = document.createElement('div');
     div.innerHTML = `
-      <button onclick="handleLoadPost('${category.category_id}')" class="tab mx-1 rounded-lg text-center active:bg-primary active:text-white focus:bg-primary focus:text-white transition-colors duration-200">${category.category}</button>
+      <button onclick="handleLoadPost('${category.id}','${page}')" class="tab mx-1 rounded-lg text-center active:bg-primary active:text-white focus:bg-primary focus:text-white transition-colors duration-200">${category.category}</button>
     `;
     categoryContainer.appendChild(div);
   });
@@ -22,58 +57,46 @@ const handleCategory = async () => {
 
 // Post section
 
-const handleLoadPost = async (postId) => {
-  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${postId}`);
+const handleLoadPost = async (postId,page) => {
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=bab42df8cfdaa259b0245a26fd039f95&page=${page}&with_genres=${postId}&per_page=10`);
 
   const data = await response.json();
-  categories = data.data;
+  categories = data.results;
 
   const cardContainer = document.getElementById('card-container');
   cardContainer.innerHTML = '';
-
+console.log(categories)
   const cardContainer2 = document.getElementById('card-container2');
   cardContainer2.innerHTML = '';
-
-  if (Array.isArray(categories) && categories.length > 0) {
+// ${convertTime(post?.others?.posted_date ? post?.others?.posted_date : '' )}
+  if (categories.length > 0) {
     categories.forEach((post) => {
       const div = document.createElement('div');
       div.innerHTML = `
         <div class="card card-compact bg-base-100 shadow-xl">
-          <figure><img class="w-80 h-52" src="${post?.thumbnail}" alt="Loading"  /> 
+          <figure><img class="w-80" src="https://image.tmdb.org/t/p/w500/${post?.poster_path}" alt="Loading"  /> 
           
           <div class="absolute  mt-40 ml-32 text-white text-xs  font-bold bg-black bg-opacity-50">
-          ${convertTime(post?.others?.posted_date ? post?.others?.posted_date : '' )}
+         ${post?.release_date}
         </div>
           
           </figure>
           <div class="card-body">
             <div class="flex items-center gap-4">
-              <img class="w-10 h-10 rounded-full" src="${post?.authors[0]?.profile_picture}" alt="">
               <span class="font-bold text-base">${post?.title}</span>
             </div>
             <div class="flex items-center gap-4 mx-14">
-              <h2 class="font-normal text-sm">${post?.authors[0]?.profile_name}</h2>
-              ${post?.authors[0]?.verified ? '<img src="./images/fi_10629607.svg" alt="">' : ''}
+              <h2 class="font-normal text-sm"></h2>
             </div>
             <div class="card-actions mx-14">
-              <p>${post?.others?.views} Views</p>
+              <p>popularity ${post?.popularity}</p>
             </div>
           </div>
         </div>
       `;
       cardContainer.appendChild(div);
-
-      function convertTime(seconds) {
-         if ( seconds === '') {
-          return '';
-        } 
-      
-        const hours = Math.floor(seconds / 3600);
-        const Seconds = seconds % 3600;
-        const minutes = Math.floor(Seconds / 60);
-      
-        return `${hours} hours ${minutes} minutes ago`;
-      }
+      pageNoP.textContent = `Page No ${page}` ;
+      nextPageBtn.id = postId;
     
 
 
@@ -94,7 +117,7 @@ const handleLoadPost = async (postId) => {
 const shortBtn = document.getElementById('sortBtn');
 
 const btnHandler = () => {
-  const sortedCategories = [...categories].sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views));
+  const sortedCategories = [...categories].sort((a, b) => parseInt(b.popularity) - parseInt(a.popularity));
   const cardContainer = document.getElementById('card-container');
   cardContainer.innerHTML = '';
 
@@ -102,42 +125,29 @@ const btnHandler = () => {
     sortedCategories.forEach((post) => {
       const div = document.createElement('div');
       div.innerHTML = `
-        <div class="card card-compact bg-base-100 shadow-xl">
-          <figure><img class="w-80 h-52" src="${post?.thumbnail}" alt="Loading" />
-        
+       <div class="card card-compact bg-base-100 shadow-xl">
+          <figure><img class="w-80" src="https://image.tmdb.org/t/p/w500/${post?.poster_path}" alt="Loading"  /> 
+          
           <div class="absolute  mt-40 ml-32 text-white text-xs  font-bold bg-black bg-opacity-50">
-          ${convertTime(post?.others?.posted_date ? post?.others?.posted_date  : '' )}
+         ${post?.release_date}
         </div>
           
           </figure>
           <div class="card-body">
             <div class="flex items-center gap-4">
-              <img class="w-10 h-10 rounded-full" src="${post?.authors[0]?.profile_picture}" alt="">
               <span class="font-bold text-base">${post?.title}</span>
             </div>
             <div class="flex items-center gap-4 mx-14">
-              <h2 class="font-normal text-sm">${post?.authors[0]?.profile_name}</h2>
-              ${post?.authors[0]?.verified ? '<img src="./images/fi_10629607.svg" alt="">' : ''}
+              <h2 class="font-normal text-sm"></h2>
             </div>
             <div class="card-actions mx-14">
-              <p>${post?.others?.views} Views</p>
+              <p>popularity ${post?.popularity}</p>
             </div>
           </div>
         </div>
       `;
       cardContainer.appendChild(div);
 
-      function convertTime(seconds) {
-        if ( seconds === '') {
-         return '';
-       } 
-     
-       const hours = Math.floor(seconds / 3600);
-       const Seconds = seconds % 3600;
-       const minutes = Math.floor(Seconds / 60);
-     
-       return `${hours} hours ${minutes} minutes ago`;
-     }
     });
   }
 };
@@ -147,4 +157,4 @@ shortBtn.addEventListener('click', btnHandler);
 
 
 handleCategory();
-handleLoadPost(1000);
+handleLoadPost(' ','1');
