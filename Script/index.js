aef7f090dcd6626e346d87bc449e3e0e2b041866
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-11-08 20:34:05 +0600

    added php codes for some security issue

--- Script/index.js
+++ Script/index.js
@@ -1,32 +1,30 @@
-let categories = [];
-
 // Tab section
 let page = 1;
 let pageNoP = document.querySelector(".page-no");
 let nextPageBtn = document.querySelector(".next-page-btn");
 let prevPageBtn = document.querySelector(".prev-page-btn");
-let generId = ' ';
-nextPageBtn.addEventListener("click",(e)=>{
+let generId = "";
+nextPageBtn.addEventListener("click",function(e){
   if(page <= 1){
     prevPageBtn.disabled = true;
   }else{
     prevPageBtn.disabled = false;
   }
-  page ++;
+  page += 1;
   handleLoadPost(e.target.id,page);
-})
-prevPageBtn.addEventListener("click",(e)=>{
+});
+prevPageBtn.addEventListener("click",function(e){
   if(page <= 1){
     prevPageBtn.disabled = true;
   }else{
     prevPageBtn.disabled = false;
   }
-  page --;
+  page -= 1;
   handleLoadPost(e.target.id,page);
-})
-const handleCategory = async () => {
+});
+const handleCategory =  function () {
 
-  categories = [{
+  const categories = [{
     category:"Action",
     id:28
   },
@@ -41,47 +39,57 @@ const handleCategory = async () => {
    {
     category:"Adventure",
     id:12
-  },
+  }
   ];
 
   const categoryContainer = document.getElementById("category-container");
 
-  categories.slice(0, 4).forEach((category) => {
-    const div = document.createElement('div');
+  categories.slice(0, 4).forEach(function(category) {
+    const div = document.createElement("div");
     div.innerHTML = `
-      <button onclick="handleLoadPost('${category.id}','${page}')" class="tab mx-1 rounded-lg text-center active:bg-primary active:text-white focus:bg-primary focus:text-white transition-colors duration-200">${category.category}</button>
+      <button onclick="handleLoadPost('${category.id}','${page}')"
+      class="tab mx-1 rounded-lg text-center active:bg-primary
+      active:text-white focus:bg-primary focus:text-white
+      transition-colors duration-200">${category.category}
+      </button>
     `;
     categoryContainer.appendChild(div);
   });
 };
-const apiKey = process.env.tmdb_api_key;
-console.log(apiKey);
+
 
 
 
 // movie list section
 
-const handleLoadPost = async (postId,page) => {
+const handleLoadPost = async function(postId,page)  {
   generId = postId;
-  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=bab42df8cfdaa259b0245a26fd039f95y&page=${page}&with_genres=${generId}&per_page=10`);
-  const data = await response.json();
-  categories = data.results;
+  const response = await fetch(`req.php?
+  page=${page}&
+  generId=${generId}&
+  per_page=10`);
+  let data = await response.json();
+  data = JSON.parse(data);
+  let categories = data.results;
 
-  const cardContainer = document.getElementById('card-container');
-  cardContainer.innerHTML = '';
-  const cardContainer2 = document.getElementById('card-container2');
-  cardContainer2.innerHTML = '';
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  const cardContainer2 = document.getElementById("card-container2");
+  cardContainer2.innerHTML = "";
   if (categories.length > 0) {
-    categories.forEach((post) => {
-      const div = document.createElement('div');
+    categories.forEach(function(post) {
+      const div = document.createElement("div");
       div.innerHTML = `
        <div class="card card-compact bg-base-100 shadow-xl relative">
-          <figure><img class="w-80" src="https://image.tmdb.org/t/p/w500/${post?.poster_path}" alt="Loading"  /> 
-          
-          <div class="absolute  mt-40 ml-32 text-white text-xs  font-bold bg-black bg-opacity-50">
+          <figure>
+          <img class="w-80"
+          src="https://image.tmdb.org/t/p/w500/${post?.poster_path}"
+          alt="Loading"  />
+          <div
+          class="absolute  mt-40 ml-32 text-white text-xs
+          font-bold bg-black bg-opacity-50">
          ${post?.release_date}
-        </div>
-          
+       </div>
           </figure>
           <div class="card-body cursor-pointer">
             <div class="flex items-center gap-4">
@@ -93,11 +101,13 @@ const handleLoadPost = async (postId,page) => {
             <div class="card-actions mx-14">
               <p>popularity ${post?.popularity}</p>
             </div>
-            <div class="card-body-hover w-full h-full bg-black absolute text-white top-0 left-0 rounded text-center cursor-pointer">
-                <div class="flex items-center gap-4 justify-center flex-col p-2">
+            <div class="card-body-hover w-full h-full bg-black absolute
+            text-white top-0 left-0 rounded text-center cursor-pointer">
+                <div class="flex items-center gap-4
+                justify-center flex-col p-2">
                   <h2 class="text-2xl">${post?.title}</h2>
                   <p>${post?.overview}</p>
-                </div>         
+                </div>
           </div>
           </div>
         </div>
@@ -105,7 +115,7 @@ const handleLoadPost = async (postId,page) => {
       cardContainer.appendChild(div);
       pageNoP.textContent = `Page No ${page}` ;
       nextPageBtn.id = postId;
-    
+
 
 
     });
@@ -115,9 +125,10 @@ const handleLoadPost = async (postId,page) => {
     <div  class="flex justify-center flex-col text-center items-center">
     <img class="w-36 h-36" src="./images/Icon.png" alt="">
     <br>
-    <p class="text-4xl font-bold text-center ">Oops!! Sorry, There is no <br> content here</p>
-    
-    </div>
+    <p class="text-4xl font-bold text-center ">
+    Oops!! Sorry, There is no
+    <br>
+    content here</p> </div>
     `;
   }
   if(page <= 1){
@@ -125,55 +136,61 @@ const handleLoadPost = async (postId,page) => {
   }
 };
 
-const shortBtn = document.getElementById('sortBtn');
+const shortBtn = document.getElementById("sortBtn");
 
-const btnHandler = (postId,page) => {
-  const sortedCategories = [...categories].sort((a, b) => parseInt(b.popularity) - parseInt(a.popularity));
-  const cardContainer = document.getElementById('card-container');
-  cardContainer.innerHTML = '';
+// const btnHandler = function(postId,page) {
+//   const sortedCategories = 
+//         [...categories].sort((a, b) =>
+//        parseInt(b.popularity) - parseInt(a.popularity));
+//   const cardContainer = document.getElementById("card-container");
+//   cardContainer.innerHTML = "";
 
-  if (Array.isArray(sortedCategories)) {
-    sortedCategories.forEach((post) => {
-      const div = document.createElement('div');
-      div.innerHTML = `
-       <div class="card card-compact bg-base-100 shadow-xl relative">
-          <figure><img class="w-80" src="https://image.tmdb.org/t/p/w500/${post?.poster_path}" alt="Loading"  /> 
-          
-          <div class="absolute  mt-40 ml-32 text-white text-xs  font-bold bg-black bg-opacity-50">
-         ${post?.release_date}
-        </div>
-          
-          </figure>
-          <div class="card-body cursor-pointer">
-            <div class="flex items-center gap-4">
-              <span class="font-bold text-base">${post?.title}</span>
-            </div>
-            <div class="flex items-center gap-4 mx-14">
-              <h2 class="font-normal text-sm"></h2>
-            </div>
-            <div class="card-actions mx-14">
-              <p>popularity ${post?.popularity}</p>
-            </div>
-            <div class="card-body-hover w-full h-full bg-black absolute text-white top-0 left-0 rounded text-center cursor-pointer">
-                <div class="flex items-center gap-4 justify-center flex-col p-2">
-                  <h2 class="text-2xl">${post?.title}</h2>
-                  <p>${post?.overview}</p>
-                </div>         
-          </div>
-          </div>
-        </div>
-      `;
-      cardContainer.appendChild(div);
-      pageNoP.textContent = `Page No ${page}` ;
-      nextPageBtn.id = postId;
+//   if (Array.isArray(sortedCategories)) {
+//     sortedCategories.forEach(function(post) {
+//       const div = document.createElement("div");
+//       div.innerHTML = `
+//        <div class="card card-compact bg-base-100 shadow-xl relative">
+//           <figure><img class="w-80"
+//           src="https://image.tmdb.org/t/p/w500/${post?.poster_path}"
+//           alt="Loading"  />
+//           <div
+//           class="absolute  mt-40 ml-32 text-white text-xs
+//           font-bold bg-black bg-opacity-50">
+//          ${post?.release_date}
+//         </div>
+//           </figure>
+//           <div class="card-body cursor-pointer">
+//             <div class="flex items-center gap-4">
+//               <span class="font-bold text-base">${post?.title}</span>
+//             </div>
+//             <div class="flex items-center gap-4 mx-14">
+//               <h2 class="font-normal text-sm"></h2>
+//             </div>
+//             <div class="card-actions mx-14">
+//               <p>popularity ${post?.popularity}</p>
+//             </div>
+//           <div
+//           class="card-body-hover w-full h-full bg-black absolute
+//           text-white top-0 left-0 rounded text-center cursor-pointer">
+//               <div class="flex items-center gap-4 justify-center flex-col p-2">
+//                   <h2 class="text-2xl">${post?.title}</h2>
+//                   <p>${post?.overview}</p>
+//                 </div>
+//           </div>
+//           </div>
+//         </div>
+//       `;
+//       cardContainer.appendChild(div);
+//       pageNoP.textContent = `Page No ${page}` ;
+//       nextPageBtn.id = postId;
 
-    });
-  }
-};
+//     });
+//   }
+// };
 
-shortBtn.addEventListener('click', btnHandler(generId,page));
+// shortBtn.addEventListener("click", btnHandler(generId,page));
 
 
 
 handleCategory();
-handleLoadPost(' ','1');
+handleLoadPost(" ","1");
---
+++ req.php
@@ -0,0 +1,9 @@
+<?php
+$apiKey = "bab42df8cfdaa259b0245a26fd039f95";
+if(isset($_GET['page']) && isset($_GET['generId'])){
+	$page = $_GET['page'];
+	$generId = $_GET['generId'] ;
+	$url = "https://api.themoviedb.org/3/discover/movie?api_key=$apiKey&page=$page&with_genres=$generId&er_page=10";
+	$data = file_get_contents($url);
+	echo json_encode($data);
+}
